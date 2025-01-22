0f903ecb8303dc3fea6b81478db2b34b4b656564
Julius Torralba <Julius.Torralba@GMail.com>
2025-01-22 13:26:57 -0700

    Disable Swagger.

--- SignalR/Program.cs
+++ SignalR/Program.cs
@@ -34,16 +34,17 @@ namespace SignalR
             // Add services to the container.
             builder.Services.AddAuthorization();
 
+            //JTorralba - Disable Swagger
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
+            //builder.Services.AddEndpointsApiExplorer();
 
-            //JTorralba
+            //JTorralba - Disable Swagger
             //builder.Services.AddSwaggerGen();
-            builder.Services.AddSwaggerGen(opt => opt.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
-            {
-                Title = Title,
-                Description = $"{Title} (SignalR WebAPI)"
-            }));
+            //builder.Services.AddSwaggerGen(opt => opt.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+            //{
+            //    Title = Title,
+            //    Description = $"{Title} (SignalR WebAPI)"
+            //}));
 
             //JTorralba
             builder.Services.AddResponseCompression(options =>
@@ -69,8 +70,9 @@ namespace SignalR
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                //JTorralba - Disable Swagger
+                //app.UseSwagger();
+                //app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
@@ -85,20 +87,21 @@ namespace SignalR
                 "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
             };
 
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast")
-            .WithOpenApi();
+            //JTorralba - Disable Swagger
+            //app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            //{
+            //    var forecast = Enumerable.Range(1, 5).Select(index =>
+            //        new WeatherForecast
+            //        {
+            //            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            //            TemperatureC = Random.Shared.Next(-20, 55),
+            //            Summary = summaries[Random.Shared.Next(summaries.Length)]
+            //        })
+            //        .ToArray();
+            //    return forecast;
+            //})
+            //.WithName("GetWeatherForecast")
+            //.WithOpenApi();
 
             app.Run();
         }
